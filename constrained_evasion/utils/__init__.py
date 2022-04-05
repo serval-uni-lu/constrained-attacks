50ded80b664448f39995009a273d82fbd08aa2d7
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2022-04-05 14:51:28 +0200

    Correct cut in batch function

--- constrained_evasion/utils/__init__.py
+++ constrained_evasion/utils/__init__.py
@@ -27,8 +27,7 @@ def cut_in_batch(arr, n_desired_batch=1, batch_size=None):
     if batch_size is None:
         n_batch = min(n_desired_batch, len(arr))
     else:
-        n_batch = np.ceil(len(arr / batch_size))
-
+        n_batch = np.ceil(len(arr) / batch_size)
     batches_i = np.array_split(np.arange(arr.shape[0]), n_batch)
 
     return [arr[batch_i] for batch_i in batches_i]
