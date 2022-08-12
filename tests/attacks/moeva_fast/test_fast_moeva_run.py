04a9c3dacd8920c22b401c13daf57527668d085d
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2022-08-12 16:57:27 +0200

    Test that the population gets updated over generations

--- tests/attacks/moeva_fast/test_fast_moeva_run.py
+++ tests/attacks/moeva_fast/test_fast_moeva_run.py
@@ -173,3 +173,51 @@ def test_run_without_history(model, n_batch):
         x_clean, y_clean, batch_size=batch_size, return_history=False
     )
     assert x_adv.shape == (n_input, attack.n_pop + 3, x_clean.shape[-1])
+
+
+def test_generating():
+    n_batch = 1
+    constraints = get_url_constraints()
+    n_input = 10
+    batch_size = n_input / n_batch
+    n_gen = 50
+    n_pop = 203
+
+    x_clean = np.load("tests/resources/url/baseline_X_test_candidates.npy")
+    y_clean = np.load("tests/resources/url/baseline_y_test_candidates.npy")
+
+    x_clean = np.repeat(x_clean, np.ceil(n_input / x_clean.shape[0]), axis=0)[
+        :n_input
+    ]
+    y_clean = np.repeat(y_clean, np.ceil(n_input / y_clean.shape[0]), axis=0)[
+        :n_input
+    ]
+
+    model = joblib.load("./tests/resources/url/baseline_rf.model")
+    preprocessing_pipeline = joblib.load(
+        "./tests/resources/url/baseline_scaler.joblib"
+    )
+    model_pipeline = Pipeline(
+        steps=[("preprocessing", preprocessing_pipeline), ("model", model)]
+    )
+
+    attack = Moeva2(
+        model_pipeline,
+        constraints,
+        2,
+        preprocessing_pipeline.transform,
+        n_gen=n_gen,
+        n_pop=n_pop,
+        save_history="full",
+        seed=42,
+        n_jobs=1,
+    )
+    x_adv = attack.generate(
+        x_clean, y_clean, batch_size=batch_size, return_history=False
+    )
+
+    diff = np.abs(x_adv - x_clean[:, np.newaxis, :]) > 0
+    diff_var = np.max(diff, axis=-1)
+    diff_pop = np.max(diff_var, axis=-1)
+    diff_total = np.sum(diff_pop)
+    assert len(x_adv) == diff_total
