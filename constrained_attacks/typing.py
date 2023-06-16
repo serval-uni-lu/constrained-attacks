ac114b663ecbdd6bcfcdf5f4d28c13361c709a98
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2023-06-16 17:47:21 +0200

    Typing correction

--- constrained_attacks/typing.py
+++ constrained_attacks/typing.py
@@ -3,7 +3,7 @@ from typing import Union
 import numpy as np
 import numpy.typing as npt
 
-NDANumber = npt.NDArray[Union[np.float_, np.int_]]
+NDNumber = npt.NDArray[Union[np.float_, np.int_]]
 NDFloat = npt.NDArray[np.float_]
 NDInt = npt.NDArray[np.int_]
 NDBool = npt.NDArray[np.bool_]
