Fixed L2 distance computation for Tabular data

constrained_attacks/attacks/cta/capgd.py
@@ -200,6 +200,9 @@ class CAPGD(Attack):
             )
         elif self.norm == "L2":
             t = torch.randn(x.shape).to(self.device).detach()
+            if self.mutable_mask.shape[0] <x.shape[1]:
+                self.mutable_mask = torch.cat((self.mutable_mask, torch.ones(1,).to(self.mutable_mask.device)))
+
             x_adv = x.detach() + self.mutable_mask * (
                     self.eps
                     * torch.ones(
@@ -211,7 +214,7 @@ class CAPGD(Attack):
                     .to(self.device)
                     .detach()
                     * t
-                    / ((t ** 2).sum(dim=(1, 2, 3), keepdim=True).sqrt() + 1e-12)
+                    / ((t ** 2).sum(dim=list(range(1,len(x.shape))), keepdim=True).sqrt() + 1e-12)
             )
         x_adv = x_adv.clamp(0.0, 1.0)
         x_best = x_adv.clone()
@@ -328,7 +331,7 @@ class CAPGD(Attack):
                             * grad
                             / (
                                     (grad ** 2 * self.mutable_mask)
-                                    .sum(dim=(1, 2, 3), keepdim=True)
+                                    .sum(dim=list(range(1,len(x.shape))), keepdim=True)
                                     .sqrt()
                                     + 1e-12
                             )
@@ -338,7 +341,7 @@ class CAPGD(Attack):
                         + (x_adv_1 - x)
                         / (
                                 ((x_adv_1 - x) ** 2)
-                                .sum(dim=(1, 2, 3), keepdim=True)
+                                .sum(dim=list(range(1,len(x.shape))), keepdim=True)
                                 .sqrt()
                                 + 1e-12
                         )
@@ -346,7 +349,7 @@ class CAPGD(Attack):
                             self.eps
                             * torch.ones(x.shape).to(self.device).detach(),
                             ((x_adv_1 - x) ** 2)
-                            .sum(dim=(1, 2, 3), keepdim=True)
+                            .sum(dim=list(range(1,len(x.shape))), keepdim=True)
                             .sqrt(),
                         ),
                         0.0,
@@ -358,7 +361,7 @@ class CAPGD(Attack):
                         + (x_adv_1 - x)
                         / (
                                 ((x_adv_1 - x) ** 2)
-                                .sum(dim=(1, 2, 3), keepdim=True)
+                                .sum(dim=list(range(1,len(x.shape))), keepdim=True)
                                 .sqrt()
                                 + 1e-12
                         )
@@ -366,7 +369,7 @@ class CAPGD(Attack):
                             self.eps
                             * torch.ones(x.shape).to(self.device).detach(),
                             ((x_adv_1 - x) ** 2)
-                            .sum(dim=(1, 2, 3), keepdim=True)
+                            .sum(dim=list(range(1,len(x.shape))), keepdim=True)
                             .sqrt()
                             + 1e-12,
                         ),

constrained_attacks/attacks/cta/cmapgd.py
@@ -191,7 +191,7 @@ class CMAPGD(Attack):
                     .to(self.device)
                     .detach()
                     * t
-                    / ((t ** 2).sum(dim=(1, 2, 3), keepdim=True).sqrt() + 1e-12)
+                    / ((t ** 2).sum(dim=list(range(1,len(x.shape))), keepdim=True).sqrt() + 1e-12)
             )
         x_adv = x_adv.clamp(0.0, 1.0)
         x_best = x_adv.clone()
@@ -311,7 +311,7 @@ class CMAPGD(Attack):
                             * grad
                             / (
                                     (grad ** 2 * self.mutable_mask)
-                                    .sum(dim=(1, 2, 3), keepdim=True)
+                                    .sum(dim=list(range(1,len(x.shape))), keepdim=True)
                                     .sqrt()
                                     + 1e-12
                             )
@@ -321,7 +321,7 @@ class CMAPGD(Attack):
                         + (x_adv_1 - x)
                         / (
                                 ((x_adv_1 - x) ** 2)
-                                .sum(dim=(1, 2, 3), keepdim=True)
+                                .sum(dim=list(range(1,len(x.shape))), keepdim=True)
                                 .sqrt()
                                 + 1e-12
                         )
@@ -329,7 +329,7 @@ class CMAPGD(Attack):
                             self.eps
                             * torch.ones(x.shape).to(self.device).detach(),
                             ((x_adv_1 - x) ** 2)
-                            .sum(dim=(1, 2, 3), keepdim=True)
+                            .sum(dim=list(range(1,len(x.shape))), keepdim=True)
                             .sqrt(),
                         ),
                         0.0,
@@ -341,7 +341,7 @@ class CMAPGD(Attack):
                         + (x_adv_1 - x)
                         / (
                                 ((x_adv_1 - x) ** 2)
-                                .sum(dim=(1, 2, 3), keepdim=True)
+                                .sum(dim=list(range(1,len(x.shape))), keepdim=True)
                                 .sqrt()
                                 + 1e-12
                         )
@@ -349,7 +349,7 @@ class CMAPGD(Attack):
                             self.eps
                             * torch.ones(x.shape).to(self.device).detach(),
                             ((x_adv_1 - x) ** 2)
-                            .sum(dim=(1, 2, 3), keepdim=True)
+                            .sum(dim=list(range(1,len(x.shape))), keepdim=True)
                             .sqrt()
                             + 1e-12,
                         ),