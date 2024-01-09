df08860edbb1ce3e80704c9e3c4444dbeadb744c
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2024-01-09 11:20:06 +0100

    New logger

--- comet/__init__.py
+++ comet/__init__.py
@@ -1,22 +1,52 @@
-from mlc.logging.comet_config import COMET_APIKEY, COMET_PROJECT, COMET_WORKSPACE
-from comet_ml import Experiment
+import json
+import os
+from pathlib import Path
+import uuid
+from mlc.logging.comet_config import (
+    COMET_APIKEY,
+    COMET_PROJECT,
+    COMET_WORKSPACE,
+)
+from comet_ml import Experiment, OfflineExperiment
 import time
+import uuid
+
 
 class XP(object):
-    def __init__(self,args, project_name=COMET_PROJECT, workspace=COMET_WORKSPACE):
-
+    def __init__(
+        self, args, project_name=COMET_PROJECT, workspace=COMET_WORKSPACE
+    ):
         if project_name == "" or project_name == None:
             self.xp = None
         else:
             timestamp = time.time()
             args["timestamp"] = timestamp
-            experiment_name = "mlc_{}_{}_{}_{}".format(args.get("model_name", ""), args.get("dataset_name", ""),
-                                                           args.get("attack_name", ""),timestamp)
-            experiment = Experiment(api_key=COMET_APIKEY,
-                                    project_name=project_name,
-                                    workspace=workspace,
-                                    auto_param_logging=False, auto_metric_logging=False,
-                                    parse_args=False, display_summary=False, disabled=False)
+            experiment_name = "mlc_{}_{}_{}_{}".format(
+                args.get("model_name", ""),
+                args.get("dataset_name", ""),
+                args.get("attack_name", ""),
+                timestamp,
+            )
+            experiment = Experiment(
+                api_key=COMET_APIKEY,
+                project_name=project_name,
+                workspace=workspace,
+                auto_param_logging=False,
+                auto_metric_logging=False,
+                parse_args=False,
+                display_summary=False,
+                disabled=False,
+            )
+
+            experiment = OfflineExperiment(
+                project_name=project_name,
+                workspace=workspace,
+                auto_param_logging=False,
+                auto_metric_logging=False,
+                parse_args=False,
+                display_summary=False,
+                disabled=False,
+            )
 
             experiment.set_name(experiment_name)
             experiment.log_parameters(args)
@@ -25,22 +55,21 @@ class XP(object):
 
     def log_parameters(self, *args, **kwargs):
         if self.xp is not None:
-            self.xp.log_parameters(*args,**kwargs)
+            self.xp.log_parameters(*args, **kwargs)
         else:
             print("logging parameters", *args)
             print("logging parameters", kwargs)
 
-
     def log_metrics(self, *args, **kwargs):
         if self.xp is not None:
-            self.xp.log_metrics(*args,**kwargs)
+            self.xp.log_metrics(*args, **kwargs)
         else:
             print("logging metrics", *args)
             print("logging metrics", kwargs)
 
     def log_metric(self, name, value, **kwargs):
         if self.xp is not None:
-            self.xp.log_metric(name, value,**kwargs)
+            self.xp.log_metric(name, value, **kwargs)
         else:
             print("logging metric", name, value)
             print("logging metric", kwargs)
@@ -49,13 +78,13 @@ class XP(object):
         if self.xp is not None:
             self.xp.end()
 
-    def log_model(self, name,path):
+    def log_model(self, name, path):
         if self.xp is not None:
             self.xp.log_model(name, path, copy_to_tmp=False)
         else:
             print("Logging model", name, path)
 
-    def log_asset(self, name,path):
+    def log_asset(self, name, path):
         if self.xp is not None:
             self.xp.log_asset(name, path, copy_to_tmp=False)
         else:
@@ -63,13 +92,80 @@ class XP(object):
 
     def get_name(self):
         if self.xp is not None:
-            return self.xp.get_name()+"_"
+            return self.xp.get_name() + "_"
         else:
             return ""
-
 
     def flush(self):
         if self.xp is not None:
             return self.xp.flush()
         else:
             return None
+
+
+class LocalXp:
+    def __init__(
+        self, args, project_name=COMET_PROJECT, workspace=COMET_WORKSPACE
+    ):
+        # Add a timestamp
+        timestamp = time.time()
+        args["timestamp"] = timestamp
+        args["uuid"] = str(uuid.uuid4())[:8]
+
+        self.experiment_name = "mlc_{}_{}_{}_{}_{}".format(
+            args.get("model_name", ""),
+            args.get("dataset_name", ""),
+            args.get("attack_name", ""),
+            timestamp,
+            args.get("uuid", ""),
+        )
+        self.parameters = {}
+        self.metrics = {}
+
+        self.log_parameters(args)
+
+        self.workspace = workspace
+        self.project_name = project_name
+        self.path = os.path.join(
+            os.getenv("XP_ROOT", "./data/xp"),
+            self.workspace,
+            self.project_name,
+        )
+
+    def log_parameters(self, *args, **kwargs):
+        self.parameters.update(kwargs)
+
+    def log_metrics(self, *args, **kwargs):
+        self.metrics.update(kwargs)
+
+    def get_name(self):
+        return self.experiment_name + "_"
+
+    def log_metric(self, name, value, **kwargs):
+        self.log_metrics(**{name: value})
+
+    def log_model(self, name, path):
+        pass
+
+    def log_asset(self, name, path):
+        pass
+
+    def save_json(self):
+        Path(self.path).mkdir(parents=True, exist_ok=True)
+        with open(
+            os.path.join(self.path, self.experiment_name + ".json"), "w"
+        ) as f:
+            f.write(
+                json.dumps(
+                    {
+                        "parameters": self.parameters,
+                        "metrics": self.metrics,
+                    }
+                )
+            )
+
+    def end(self):
+        self.save_json()
+
+    def flush(self):
+        self.save_json()
